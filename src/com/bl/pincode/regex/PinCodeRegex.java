cc68be75c33b62c5686410842ff426e6ce209540
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 19:26:23 +0530

    Added:Create Regex pattern to Validate PinCode

--- src/com/bl/pincode/regex/PinCodeRegex.java
+++ src/com/bl/pincode/regex/PinCodeRegex.java
@@ -1,7 +1,32 @@
 package com.bl.pincode.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCodeRegex {
-	public static void main(String[] args) {
-		System.out.println("Welcome In PinCode regex Program");
+
+	public static boolean checkValidPincode(String pinCode) {
+
+		String pinCodeRegex = "[0-9]{6}$";
+		Pattern patternObject = Pattern.compile(pinCodeRegex);
+		if (pinCode == null) {
+			return false;
+		}
+		Matcher matcherObject = patternObject.matcher(pinCode);
+		return matcherObject.matches();
 	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome In PinCode regex Program");
+
+		String pinCode = "400088";
+		boolean isPinCode = checkValidPincode(pinCode);
+
+		if (isPinCode)
+			System.out.println(pinCode + " is an Valid PinCode");
+		else
+			System.out.println(pinCode + " is an Invalid PinCode");
+	}
+
 }
